503d3702bcdcbaf8c3b560c66e683c9daa6f647d
Chenyang Fang <fang524676125@outlook.com>
2020-11-12 23:47:59 -0800

    convert to function comp and set up hook

--- frontend/src/containers/googleLogin.js
+++ frontend/src/containers/googleLogin.js
@@ -1,72 +1,72 @@
 
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { useHistor} from "react-router-dom";
 
+import './Login.css'
 
 const CLIENT_ID = '117590776103-qt4jgq89g0vhbeu72v4vja56s6sti0as.apps.googleusercontent.com';
 
 
-class GoogleButton extends Component {
-   constructor(props) {
-    super(props);
+function GoogleButton(props) {
 
-    this.state = {
-      isLogined: false,
-      accessToken: '',
-      email: ''
-    };
+    const [isLogined, setLogin] = useState(false);
+    const [accessToken, setToken] = useState("");
+    const [email, setEmail] = useState("");
 
-  }
+    let history = useHistory();
 
-  login = (response) => {
-    if(response.accessToken){
-      this.setState(state => ({
-        isLogined: true,
-        accessToken: response.accessToken,
-        email:response.getBasicProfile().getEmail()
-      }));
+    const login = (response) => {
+        
+        // either sent to server to verify lion email or we check here 
+        // TODO: verify the email is lionmails
+        //history.push('/tmp', {name: 'Hello'});
+        // GOTO another page 
+
     }
-  }
 
-  logout = (response) => {
-    this.setState(state => ({
-      isLogined: false,
-      accessToken: ''
-    }));
-  }
+    const logout = (response) => {
+        setLogin(false);
+    }
 
-  handleLoginFailure = (response) => {
-    alert('Failed to log in')
-  }
+    const handleLoginFailure = (response) => {
+        alert('Failed to log in')
+    }
 
-  handleLogoutFailure = (response) => {
-    alert('Failed to log out')
-  }
+    const handleLogoutFailure = (response) => {
+        alert('Failed to log out')
+    }
 
-  render() {
     return (
-    <div>
-      { this.state.isLogined ?
-        <GoogleLogout
-          clientId={ CLIENT_ID }
-          buttonText='Logout'
-          onLogoutSuccess={ this.logout }
-          onFailure={ this.handleLogoutFailure }
-        >
-        </GoogleLogout>: <GoogleLogin
-          clientId={ CLIENT_ID }
-          buttonText='Login'
-          onSuccess={ this.login }
-          onFailure={ this.handleLoginFailure }
-          cookiePolicy={ 'single_host_origin' }
-          responseType='code,token'
-        />
-      }
-      { this.state.accessToken ? <h5>Your: email: <br/><br/> { this.state.email}</h5> : null }
+        <div id="UltraBox">
+            <div id="outerBox"> 
+                <div><b id="title">AirNote</b></div>
+                <div id="gLogin">
+                    <div>
+                        { isLogined ?
+                        <GoogleLogout
+                        clientId={ CLIENT_ID }
+                        buttonText='Logout'
+                        onLogoutSuccess={ logout }
+                        onFailure={ handleLogoutFailure }
+                        >
+                        </GoogleLogout>: <GoogleLogin
+                        clientId={ CLIENT_ID }
+                        buttonText='Login with Google'
+                        onSuccess={ login }
+                        onFailure={ handleLoginFailure }
+                        cookiePolicy={ 'single_host_origin' }
+                        responseType='code,token'
+                        />
+                        }
+                        { accessToken ? <h5>Your: email: <br/><br/> { email}</h5> : null }
 
-    </div>
-    )
-  }
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+  
 }
 
 export default GoogleButton;
