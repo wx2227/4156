dba9b286d5fd7a50038132ae3ff43e29e4f4a1ce
guanguangua <guanguangua@gmail.com>
2020-11-16 18:57:01 -0500

    fixed display error from Content-Disposition

--- frontend/src/components/Preview.js
+++ frontend/src/components/Preview.js
@@ -5,9 +5,7 @@ class Preview extends React.Component {
     render() {
         const url = "https://docs.google.com/viewer?url=" + this.props.url + "&embedded=true"
         return(
-            <object data={this.props.url} type="application/pdf" style={{height: "80vh", width: "80vh"}}>
-                <iframe src={url}></iframe>
-            </object>
+            <iframe src={url} style={{height: "80vh", width: "80vh"}}>Preview</iframe>
         )
     }
 }
