d121b06008cb79efceb21e30efe1f2157ef10292
Tian Niu <niutian95@gmail.com>
2020-12-05 21:00:44 -0800

    add Preview.test.js

---
+++ frontend/src/components/Preview.test.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+
+import Preview from "./Preview";
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it("render Preview", () => {
+  act(() => {
+    render(<Preview  url={"test"}/>, container);
+  });
+  const iframe = document.querySelector("iframe");
+  expect(iframe.textContent).toBe("Preview");
+});
