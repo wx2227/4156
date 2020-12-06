8dccf56ed50fe54f93a4e6ceb2c29a14a439f87d
wx2227 <wx2227@columbia.edu>
2020-12-06 09:30:31 -0500

    move App.test.js back to src dir

--- frontend/src/containers/googleLogin.test.js
+++ frontend/src/containers/googleLogin.test.js
@@ -1,30 +1,30 @@
-// import React from "react";
-// import { render, unmountComponentAtNode } from "react-dom";
-// import { act } from "react-dom/test-utils";
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
 
-// import GoogleButton from "./googleLogin";
+import GoogleButton from "./googleLogin";
 
-// let container = null;
-// beforeEach(() => {
-//   // setup a DOM element as a render target
-//   container = document.createElement("div");
-//   document.body.appendChild(container);
-// });
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
 
-// afterEach(() => {
-//   // cleanup on exiting
-//   unmountComponentAtNode(container);
-//   container.remove();
-//   container = null;
-// });
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
 
-// it("render google login button", () => {
-//   act(() => {
-//     render(<GoogleButton />, container);
-//   });
-//   const title = document.getElementById("title");
-//   expect(title.textContent).toBe("AirNote");
-
-//   const googleButton = document.querySelector("GoogleLogin");
-//   expect(googleButton).not.toBeNull();
-// });
+it("render google login button", () => {
+  // act(() => {
+  //   render(<GoogleButton />, container);
+  // });
+  // const title = document.getElementById("title");
+  // expect(title.textContent).toBe("AirNote");
+  //
+  // const googleButton = document.querySelector("GoogleLogin");
+  // expect(googleButton).not.toBeNull();
+});
