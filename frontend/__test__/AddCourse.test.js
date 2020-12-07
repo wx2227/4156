2e6b7dbe0c264f4ae9e419da9bf8cf476a8417f7
wx2227 <wx2227@columbia.edu>
2020-12-07 02:26:38 -0500

    AddCourse with 100% coverage

--- frontend/__test__/AddCourse.test.js
+++ frontend/__test__/AddCourse.test.js
@@ -3,6 +3,14 @@ import { render, unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
 
 import AddCourse from "../src/components/AddCourse";
+import { mount, shallow } from 'enzyme'
+import AddCourseView from '../src/components/AddCourseView'
+
+jest.mock('react-router-dom', () => ({
+  useHistory: () => ({
+    push: jest.fn()
+  }),
+}));
 
 let container = null;
 beforeEach(() => {
@@ -22,14 +30,6 @@ afterEach(() => {
   container = null;
 });
 
-it("render button if admin", () => {
-  act(() => {
-    render(<AddCourse />, container);
-  });
-  const button = document.querySelector("Button");
-  expect(button.innerHTML).toBe("+ Add Course");
-});
-
 it("not render button if not admin", () => {
   Object.defineProperty(window.document, 'cookie', {
     writable: true,
@@ -41,3 +41,24 @@ it("not render button if not admin", () => {
   const button = document.querySelector("Button");
   expect(button).toBeNull();
 });
+
+describe('test add course button', () => {
+
+  it("render button if admin", () => {
+    act(() => {
+      render(<AddCourse />, container);
+    });
+    const button = document.querySelector("Button");
+    expect(button.innerHTML).toBe("+ Add Course");
+  });
+
+  it ('submit button work', () => {
+
+    const onClickSpy = jest.fn();
+    let wrapper = mount(<AddCourse onClick={onClickSpy}/>)
+
+    wrapper.find('#button').find('button').simulate('click')
+
+    expect(onClickSpy).toHaveBeenCalled()
+  })
+})
