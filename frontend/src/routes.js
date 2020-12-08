8ba013bccbbf3bed023bd40c9c8e7d053d2ba42d
wx2227 <wx2227@columbia.edu>
2020-12-08 16:50:12 -0500

    merge resolved

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-
 import NotesListView from './components/NotesListView'
 import NoteDetailView from './components/NoteDetailView'
 import UploadForm from './components/UploadForm'
